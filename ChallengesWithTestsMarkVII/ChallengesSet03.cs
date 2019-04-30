c929bfc7fe05a33e4bf7331ff11d328a3f288673
Aziza Hurt <azizamhurt@gmail.com>
2019-04-30 17:40:00 -0500

    finished my first only took an hour

--- ChallengesWithTestsMarkVII/ChallengesSet03.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet03.cs
@@ -47,7 +47,10 @@ namespace ChallengesWithTestsMarkVII
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
